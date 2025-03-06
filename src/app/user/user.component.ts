1b4b08bf562b1666ce32d6e76bb4f15fe6f2f725
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-06 18:56:40 -0500

    Add dummy user data and load random user using String Interpolation, Property Binding to access dynamic data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
 })
 export class UserComponent {
-
+  private randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  selectedUser = DUMMY_USERS[this.randomIndex];
 }
