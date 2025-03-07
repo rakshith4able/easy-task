269987451ab745aadb1dd72d1e69188548045ced
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-07 14:28:20 -0500

    create custom event to emit data from child to parent using @Output or output() with EventEmitter and $event

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,12 @@
-import { Component, computed, signal, Input } from '@angular/core';
+import {
+  Component,
+  computed,
+  signal,
+  Input,
+  Output,
+  EventEmitter,
+  output,
+} from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -8,12 +16,20 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
+
+  @Output()
+  select = new EventEmitter<string>();
+  //or
+  // select = output<string>();
 
   get imagePath() {
     return '/assets/users/' + this.avatar;
   }
 
-  onSelectUser() {}
+  onSelectUser(id: string) {
+    this.select.emit(id);
+  }
 }
