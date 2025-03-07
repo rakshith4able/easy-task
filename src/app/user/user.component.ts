c259f91191aa1c6cd4e9dbaeebbdde7187d1b739
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-06 20:22:33 -0500

    Make UserComponent reusable by using Input() decorator with options

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, signal, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -8,12 +8,12 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  private randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  selectedUser = signal(DUMMY_USERS[this.randomIndex]);
-  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  get imagePath() {
+    return '/assets/users/' + this.avatar;
   }
+
+  onSelectUser() {}
 }
