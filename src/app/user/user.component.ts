1cd98153303d56db56116ad69b644ef75235ee4a
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-06 19:04:33 -0500

    optimize imageUrl in userComponent template using getter

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -10,4 +10,8 @@ import { DUMMY_USERS } from '../dummy-users';
 export class UserComponent {
   private randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   selectedUser = DUMMY_USERS[this.randomIndex];
+
+  get imagePath() {
+    return '/assets/users/' + this.selectedUser.avatar;
+  }
 }
