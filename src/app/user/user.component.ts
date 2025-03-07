04ac58971fd72ed455cffba753483bcfca1b6739
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-06 19:33:46 -0500

    rewrite the selectedUser logic with signal and computed

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 @Component({
@@ -9,13 +9,11 @@ import { DUMMY_USERS } from '../dummy-users';
 })
 export class UserComponent {
   private randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-  selectedUser = DUMMY_USERS[this.randomIndex];
-
-  get imagePath() {
-    return '/assets/users/' + this.selectedUser.avatar;
-  }
+  selectedUser = signal(DUMMY_USERS[this.randomIndex]);
+  imagePath = computed(() => '/assets/users/' + this.selectedUser().avatar);
 
   onSelectUser() {
-    console.log('Clicked!');
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
