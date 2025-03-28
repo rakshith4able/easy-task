b232c2b65411552656fcddf2b8a8ace0abe7baea
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-28 11:50:29 -0400

    Fix the import issues

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -4,10 +4,12 @@ import { TasksComponent } from './tasks.component';
 import { NewTaskComponent } from './new-task/new-task.component';
 import { TaskComponent } from './task/task.component';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [TasksComponent, TaskComponent, NewTaskComponent],
   exports: [TasksComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, SharedModule],
 })
 export class TasksModule {}
