d04e62eff08f93d773a69e3796f0cfc18e79f1dc
Rakshith Raj G P <rakshithraj.gp11@gmail.com>
2025-03-30 20:28:48 -0400

    Implement lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
-import { resolveUserId, TasksComponent } from './tasks/tasks.component';
+import { resolveUserId } from './tasks/tasks.component';
 
 export const routes: Routes = [
   { path: '', component: NoTaskComponent },
   {
     path: 'users/:userId',
-    component: TasksComponent,
+    loadComponent: () =>
+      import('./tasks/tasks.component').then((module) => module.TasksComponent),
     resolve: { userId: resolveUserId },
   },
 ];
